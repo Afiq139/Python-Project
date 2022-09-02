eb072139f08972fc68e451cceeaa9cb866a5174a
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-09-02 23:37:41 +0800

    Create two_string_variables.py

---
+++ 2/two_string_variables.py
@@ -0,0 +1,20 @@
+# Simple Calculator
+
+
+# Write a program to take two integers as input and output their sum.
+
+# Sample Input:
+# 2
+# 8
+
+# Sample Output:
+# 10
+# Remember, input() results in a string.
+
+# your code goes here
+num1 = int(input())
+num2 = int(input())
+
+num3 = num1 + num2
+
+print(num3)
