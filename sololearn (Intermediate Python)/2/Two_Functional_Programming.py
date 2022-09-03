0136727c7b673c47fef21ea30528175119301298
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-09-03 22:50:50 +0800

    Create Two_Functional_Programming.py

---
+++ sololearn (Intermediate Python)/2/Two_Functional_Programming.py
@@ -0,0 +1,25 @@
+# Spelling Backwards
+
+
+# Given a string as input, use recursion to output each letter of the strings in reverse order, on a new line.
+
+# Sample Input
+# HELLO
+
+# Sample Output
+# O
+# L
+# L
+# E
+# H
+# Complete the recursive spell() function to produce the expected result.
+
+def spell(txt): #your code goes here
+ if txt=="": 
+  return
+ else: 
+  print(txt[-1]) 
+  return spell(txt[:-1]) 
+  
+txt = input()
+spell(txt)
