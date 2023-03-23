87f0f38020fb3eac060601d6f2bbb76fd9b411df
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-03-23 18:52:08 +0800

    Update web_scrapping.py

--- Python_Automation/web_scrapping.py
+++ Python_Automation/web_scrapping.py
@@ -39,3 +39,43 @@ content +=('<br>-----------<br>')
 content +=('<br><br>End of Message')
 
 #lets send email
+
+
+print('Composing Email...')
+
+#update your email details
+
+SERVER = 'smtp.gmail.com' #"your smtp server"
+PORT = 587 # your port number
+FROM = 'shafiq5427.engineering@gmail.com' #"your from email id"
+TO = 'shafiq5427.engineering@gmail.com' # "Your to email ids" # can be a list
+PASS = '******' #"Your email id's password"
+
+# fp = open(file_name, 'rb')
+# create a text/plain message
+# msg = MIMEText('')
+mst = MIMEultipart()
+
+#msg.add_header('Content-Disposition', 'attachment' , filename='empty.txt')
+
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+#fp.close()
+
+print('Initiating Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+#server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+#server.ehlo
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
