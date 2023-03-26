9ef6485a871e704019ee0800415f2cf8d3081c1e
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-03-26 18:24:03 +0800

    Update scratch_5.py

--- Python_Automation/scratch_5.py
+++ Python_Automation/scratch_5.py
@@ -14,3 +14,16 @@ if len(sys.argv) > 1:
     url = sys.argv[1]
 else:
     sys.exit("Error: please enter the TED Talk URL")
+
+#url = "http://www.ted.com/talks/jia_jiang_what_i_learned_from_100_days_0f_"
+#url = "http://www.ted.com/talks/ken_robinson_says_schools_kill_creativity"
+
+r = request.get(url)
+
+print("Download about to start")
+
+soup = BeautifulSoup(r.content, features="lxml")
+
+for val in soup.findAll("script"):
+    if(re.search("talkPage.init", str(val))) is not None:
+        result = str(val)
