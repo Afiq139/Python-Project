62eabddecf201a7b7c254387ccacdb216a28d562
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-07-12 00:14:19 +0800

    update

--- Pygame/Space_Invader/SpaceInvader.py
+++ Pygame/Space_Invader/SpaceInvader.py
@@ -6,9 +6,18 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800,600))
 
+# caption and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('./Pygame/Space_Invader/ufo.png')
+pygame.display.set_icon(icon)
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+    pygame.display.update()
