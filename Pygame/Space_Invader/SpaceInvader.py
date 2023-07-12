d5e9f51a0911ee5bfe0baa16886d6e4c0531d381
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-07-13 07:34:58 +0800

    Update SpaceInvader.py

--- Pygame/Space_Invader/SpaceInvader.py
+++ Pygame/Space_Invader/SpaceInvader.py
@@ -12,10 +12,10 @@ icon = pygame.image.load('./Pygame/Space_Invader/ufo.png')
 pygame.display.set_icon(icon)
 
 # Player
-
 playerImg = pygame.image.load('./Pygame/Space_Invader/player2.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 def player(x,y):
     #b blit = to draw on screen -> (image, (coordinates))
@@ -29,13 +29,27 @@ while running:
     screen.fill((0, 0, 0))
 
     # movement in pixel
-    playerY += 0.1
+    #playerY += 0.1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        #if keystroke is pressed, check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                playerX_change = 0
+
+    
+    # example: 5 = 5 + 0.1 or -0.1
+    playerX += playerX_change
+    
     player(playerX, playerY)
     pygame.display.update()
 
